885593f786a5bcddef499de94e37102c119f8a80
lucasllamanzarez <103078227+lucasllamanzarez@users.noreply.github.com>
2024-07-26 15:09:59 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Este script puede ser usado para interactividad adicional
+    // Actualmente, no se requiere script adicional para el fondo animado
+});
+
+// document.addEventListener('onclick', () => {
+        
+// });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const music = document.getElementById('background-music');
+    const musicControlButton = document.getElementById('music-control');
+    const musicIcon = document.getElementById('music-icon');
+
+    // Reproduce la música cuando la página se carga
+    music.play();
+
+    window.toggleMusic = function() {
+        if (music.paused) {
+            music.play();
+            musicIcon.classList.remove('bi-play');
+            musicIcon.classList.add('bi-pause');
+        } else {
+            music.pause();
+            musicIcon.classList.remove('bi-pause');
+            musicIcon.classList.add('bi-play');
+        }
+    };
+});
+
