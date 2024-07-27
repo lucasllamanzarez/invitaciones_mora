d7f4e2963e1ea80d049a9e788e7b48e6bf1d63c8
lucasllamanzarez <103078227+lucasllamanzarez@users.noreply.github.com>
2024-07-27 18:06:56 -0300

    se agregan nuevas secciones

--- script.js
+++ script.js
@@ -37,3 +37,46 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
+   // datos bancarios
+   document.getElementById('show-bank-details').addEventListener('click', function() {
+    Swal.fire({
+        title: 'Datos Bancarios',
+        html: `
+            <p><strong>ALIAS:</strong> morasgro</p>
+            <p><strong>CBU:</strong> 0000003100079793480010</p>
+        `,
+        icon: 'info',
+        confirmButtonText: 'Cerrar',
+        background: 'rgba(0, 0, 0, 0.4)', // Fondo opaco
+        color: '#fff', // Color del texto
+        backdrop: `rgba(0, 0, 0, 0.8)` // Fondo semi-opaco
+    });
+});
+
+//Contador
+// Fecha y hora del cumpleaños
+const eventDate = new Date('August 23, 2024 21:30:00').getTime();
+
+// Actualiza el contador cada segundo
+const countdownInterval = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = eventDate - now;
+
+    // Cálculo del tiempo restante
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Muestra el resultado en los elementos correspondientes
+    document.getElementById('countdown-days').innerText = `${days} Días`;
+    document.getElementById('countdown-hours').innerText = `${hours} Horas`;
+    document.getElementById('countdown-minutes').innerText = `${minutes} Minutos`;
+    document.getElementById('countdown-seconds').innerText = `${seconds} Segundos`;
+
+    // Si la cuenta regresiva ha terminado, muestra un mensaje
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById('countdown-timer').innerText = '¡Es Hora De Festejar!';
+    }
+}, 1000);
